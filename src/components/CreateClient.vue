daa62e77af9c2cfe8c9d9879c76cbfd1920f49f9
fedor11235 <f.avdeev@list.ru>
2022-01-27 22:10:46 +0300

    Finished the functionality and error handling of the first form

--- src/components/CreateClient.vue
+++ src/components/CreateClient.vue
@@ -1,161 +1,314 @@
 <template>
-<form>
+  <form>
+    <div>
+      <input
+        type="text"
+        name="last_name"
+        v-model.trim="$v.form.lastName.$model"
+        placeholder="Фамилия"
+      />
+      <div class="error" v-if="lastNameError">
+        <div v-for="(elem, index) in lastNameError" :key="index">
+          <span>{{ elem }}</span>
+        </div>
+      </div>
+    </div>
 
-  <div>
-    <input type="text" id="last_name" name="last_name" required placeholder=" " />
-    <label for="last_name">Фамилия</label>
-  </div>
+    <div>
+      <input
+        type="text"
+        name="first_name"
+        v-model.trim="$v.form.firstName.$model"
+        placeholder="Имя"
+      />
+      <div class="error" v-if="firstNameError">
+        <div v-for="(elem, index) in firstNameError" :key="index">
+          <span>{{ elem }}</span>
+        </div>
+      </div>
+    </div>
 
-  <div>
-    <input type="text" id="first_name" name="first_name" required placeholder=" " />
-    <label for="first_name">Имя</label>
-  </div>
+    <div>
+      <input
+        type="text"
+        name="patronymic"
+        v-model.trim="$v.form.patronymic.$model"
+        placeholder="Отчество"
+      />
+      <div class="error" v-if="patronymicError">
+        <div v-for="(elem, index) in patronymicError" :key="index">
+          <span>{{ elem }}</span>
+        </div>
+      </div>
+    </div>
 
-  <div>
-    <input type="text" id="patronymic" name="patronymic" required placeholder=" " />
-    <label for="patronymic">Отчество</label>
-  </div>
+    <div>
+      <input
+        type="date"
+        name="date"
+        v-model.trim="$v.form.date.$model"
+        placeholder="Дата Рождения"
+      />
+      <div class="error" v-if="dateError">
+        <div v-for="(elem, index) in dateError" :key="index">
+          <span>{{ elem }}</span>
+        </div>
+      </div>
+    </div>
 
-  <div>
-    <input type="text" id="date" name="date" required placeholder=" " />
-    <label for="date">Дата Рождения</label>
-  </div>
+    <div>
+      <input
+        type="tale"
+        name="tale"
+        v-model.trim="$v.form.tale.$model"
+        placeholder="Номер телефона"
+      />
+      <div class="error" v-if="taleError">
+        <div v-for="(elem, index) in taleError" :key="index">
+          <span>{{ elem }}</span>
+        </div>
+      </div>
+    </div>
 
-  <div>
-    <input type="tale" id="tale" name="tale" required placeholder=" " />
-    <label for="tale">Номер телефона</label>
-  </div>
+    <div class="check">
+      <label>Ваш пол:</label>
+      <select
+        class="select__input"
+        name="gender"
+        v-model.trim="$v.form.gender.$model"
+      >
+        <option disabled selected></option>
+        <option>Мужской</option>
+        <option>Женский</option>
+      </select>
+      <div class="error" v-if="genderError">
+        <div v-for="(elem, index) in genderError" :key="index">
+          <span>{{ elem }}</span>
+        </div>
+      </div>
+    </div>
 
-  <div class="check">
-    <label for="gender">Ваш пол:</label>
-    <select class="select__input" name="gender">
-      <option disabled selected></option>
-      <option>Мужской</option>
-      <option>Женский</option>
-    </select>
-  </div>
+    <div class="check">
+      <label>Группа клиентов:</label>
+      <select
+        class="select__input"
+        name="client-group"
+        multiple
+        v-model.trim="$v.form.clientGroup.$model"
+      >
+        <option>VIP</option>
+        <option>Проблемные</option>
+        <option>ОМС</option>
+      </select>
+      <div class="error" v-if="clientGroupError">
+        <div v-for="(elem, index) in clientGroupError" :key="index">
+          <span>{{ elem }}</span>
+        </div>
+      </div>
+    </div>
 
+    <div class="check">
+      <label>Лечащий врач:</label>
+      <select
+        class="select__input"
+        name="doctor"
+        v-model.trim="$v.form.doctor.$model"
+      >
+        <option>Иванов</option>
+        <option>Захаров</option>
+        <option>Чернышева</option>
+      </select>
 
-  <div class="check">
-    <label for="client-group">Группа клиентов:</label>
-    <select class="select__input" name="client-group" multiple >
-      <option>VIP</option>
-      <option>Проблемные</option>
-      <option>ОМС</option>
-    </select>
-  </div>
+      <div class="error" v-if="doctorError">
+        <div v-for="(elem, index) in doctorError" :key="index">
+          <span>{{ elem }}</span>
+        </div>
+      </div>
+    </div>
 
-  <div class="check">
-    <label for="doctor">Лечащий врач:</label>
-    <select class="select__input" name="doctor">
-      <option disabled selected></option>
-      <option>Иванов</option>
-      <option>Захаров</option>
-      <option>Чернышева</option>
-    </select>
-  </div>
-  
-  <div class="check">
-    <label for="sms">Не отправлять СМС:</label>
-    <input class="check__input" type="checkbox" name="sms" required placeholder=" " />
-  </div>
-  
-  <input type="submit" value="Sign Up" />
+    <div class="check">
+      <label>Не отправлять СМС:</label>
+      <div class="select__input">
+        <input
+          
+          type="checkbox"
+          name="sms"
+          v-model.trim="$v.form.sms.$model"
+        />
+      </div>
+    </div>
 
-</form>
+    <input type="submit" value="Sign Up" />
+  </form>
 </template>
 
 <script>
-import { required, minLength, between } from "vuelidate/lib/validators";
+// import { required, minLength, between } from "vuelidate/lib/validators";
+import { required, minLength} from "vuelidate/lib/validators";
 import { validationMixin } from "vuelidate";
 
 export default {
   name: "CreateClient",
   mixins: [validationMixin],
+
+
   data() {
     return {
-      name: "",
-      age: 0,
-    };
+      form: {
+        lastName: "",
+        firstName: "",
+        patronymic: "",
+        date: "",
+        tale: "",
+        gender: "",
+        clientGroup: [],
+        doctor: "",
+        sms: false,
+      }
+    }
   },
   validations: {
-    name: {
-      required,
-      minLength: minLength(4),
-    },
-    age: {
-      between: between(20, 30),
+    form: {
+      lastName: { 
+        required, 
+        minLength:minLength(2) 
+        },
+      firstName: { 
+        required,
+        minLength:minLength(2) 
+         },
+      patronymic: { 
+        required,
+        minLength:minLength(2) 
+         },
+      date: { required },
+      tale: { required },
+      gender: { required },
+      clientGroup: { required },
+      doctor: { required },
+      sms: {  },
     },
   },
-};
+
+  computed: {
+    lastNameError(){
+      let errors = [];
+      if(!this.$v.form.lastName.required) errors.push("Обязательно для заполнения!")
+      if(!this.$v.form.lastName.minLength) errors.push("Не менее двух знаков!")
+      return errors
+      
+    },
+    firstNameError(){
+      let errors = [];
+      if(!this.$v.form.firstName.required) errors.push("Обязательно для заполнения!")
+      if(!this.$v.form.firstName.minLength) errors.push("Не менее двух знаков!")
+      return errors
+    },
+
+    patronymicError(){
+      let errors = [];
+      if(!this.$v.form.patronymic.required) errors.push("Обязательно для заполнения!")
+      if(!this.$v.form.patronymic.minLength) errors.push("Не менее двух знаков!")
+      return errors
+    },
+
+    dateError(){
+      let errors = [];
+      if(!this.$v.form.date.required) errors.push("Обязательно для заполнения!")
+      if(this.$v.form.date.$model > new Date().toISOString().substring(0, 10)) errors.push("Вы указали неверную дату!")
+      return errors
+    },
+    taleError(){
+      let errors = [];
+      if(!this.$v.form.tale.required) errors.push("Обязательно для заполнения!")
+      if(!/^[\d]{11}$/.test(this.$v.form.tale.$model) && !this.$v.form.tale.$model=="") errors.push("Номер указан не верно!")
+      return errors
+    },
+
+    genderError(){
+      let errors = [];
+      if(!this.$v.form.gender.required) errors.push("Обязательно для заполнения!")
+      return errors
+    },
+
+    clientGroupError(){
+      let errors = [];
+      if(!this.$v.form.clientGroup.required) errors.push("Обязательно для заполнения!")
+      return errors
+    },
+
+    doctorError(){
+      let errors = [];
+      if(!this.$v.form.doctor.required) errors.push("Обязательно для заполнения!")
+      return errors
+    },
+  }
+}
+
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="sass" scoped>
-form 
+form
   max-width: 450px
   margin: 0 auto
 
-  > div 
-    position: relative
+  > div
     background: white
     border-bottom: 1px solid #ccc
 
-    > label 
-      opacity: 0.4
+    > label
+      float: left
+      padding: 20px 20px 20px 50px
+      opacity: 0.8
+      width: 60%
       font-weight: bold
-      position: absolute
       top: 22px
       left: 20px
 
     > input,
-      textarea
-        font-size: 1.2rem
-        padding: 20px 20px 20px 50px
-        opacity: 0.8
+    textarea
+      font-size: 1.2rem
+      opacity: 0.8
 
     > input
       width: 100%
       border: 0
       padding: 20px 20px 20px 50px
       background: #eee
-      
 
-      &:focus 
+      &:focus
         outline: 0
         background: white
-        
-        & + label 
+
+        & + label
           opacity: 0
-        
-      &:valid 
+
+      &:valid
         background: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/3/check.svg)
         background-size: 20px
         background-repeat: no-repeat
         background-position: 20px 20px
-        & + label 
+        & + label
           opacity: 0
-        
-      &:invalid:not(:focus):not(:placeholder-shown) 
+
+      &:invalid:not(:focus):not(:placeholder-shown)
         background: pink
-        & + label 
+        & + label
           opacity: 0
-        
-      &:invalid:focus:not(:placeholder-shown) 
-        & ~ .requirements 
+
+      &:invalid:focus:not(:placeholder-shown)
+        & ~ .error
           max-height: 200px
           padding: 0 30px 20px 50px
-        
-    .requirements 
+
+    .error
       padding: 0 30px 0 50px
       color: #999
-      max-height: 0
       transition: 0.28s
-      overflow: hidden
       color: red
       font-style: italic
-    
-  input[type="submit"] 
+
+  input[type="submit"]
     display: block
     width: 100%
     margin: 20px 0
@@ -166,20 +319,17 @@ form
     font-size: 1.2rem
 
   .check
-    padding: 30px
     background: white
-    >label
-      width: 100%
-      padding: 20px
-      opacity: 0.8
-      top:0
-      left:0
+
+    > .error
+
+      bottom: 0
+      left: 0
 
     >.select__input
-      position: absolute
-      right: 20px
-      top: 0
-      width: 40%
+      display: inline-block
+      position: relative
+      width: 35%
       height: 60px
       border: none
 
@@ -187,20 +337,26 @@ form
         outline: none
 
     >.select__input,
-      textarea
-        opacity: 0.8
-        font-size: 1.2rem
+    textarea
+      opacity: 0.8
+      font-size: 1.2rem
 
+    .select__input
+      >input
+        position: absolute
+        right: 0
+        top: 15px
+        width: 30px
+        height: 30px
 
     >.check__input
       position: absolute
-      margin:0
-      right:20px
+      margin: 0
+      right: 20px
       top: 15px
       width: 30px
       height: 30px
-        
-    
-* 
+
+*
   box-sizing: border-box
 </style>
