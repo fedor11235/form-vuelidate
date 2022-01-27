14899bc82fd74cc0571cfd4dedd625516d4c7047
fedor11235 <f.avdeev@list.ru>
2022-01-27 17:55:26 +0300

    Completed layout of the margins

--- src/components/CreateClient.vue
+++ src/components/CreateClient.vue
@@ -1,30 +1,63 @@
 <template>
-<form action="#0">
+<form>
+
+  <div>
+    <input type="text" id="last_name" name="last_name" required placeholder=" " />
+    <label for="last_name">Фамилия</label>
+  </div>
 
   <div>
     <input type="text" id="first_name" name="first_name" required placeholder=" " />
-    <label for="first_name">First Name</label>
+    <label for="first_name">Имя</label>
+  </div>
+
+  <div>
+    <input type="text" id="patronymic" name="patronymic" required placeholder=" " />
+    <label for="patronymic">Отчество</label>
+  </div>
+
+  <div>
+    <input type="text" id="date" name="date" required placeholder=" " />
+    <label for="date">Дата Рождения</label>
+  </div>
+
+  <div>
+    <input type="tale" id="tale" name="tale" required placeholder=" " />
+    <label for="tale">Номер телефона</label>
+  </div>
+
+  <div class="check">
+    <label for="gender">Ваш пол:</label>
+    <select class="select__input" name="gender">
+      <option disabled selected></option>
+      <option>Мужской</option>
+      <option>Женский</option>
+    </select>
+  </div>
+
+
+  <div class="check">
+    <label for="client-group">Группа клиентов:</label>
+    <select class="select__input" name="client-group" multiple >
+      <option>VIP</option>
+      <option>Проблемные</option>
+      <option>ОМС</option>
+    </select>
+  </div>
+
+  <div class="check">
+    <label for="doctor">Лечащий врач:</label>
+    <select class="select__input" name="doctor">
+      <option disabled selected></option>
+      <option>Иванов</option>
+      <option>Захаров</option>
+      <option>Чернышева</option>
+    </select>
   </div>
   
-  <div>
-    <input type="text" id="last_name" name="last_name" required placeholder=" " />
-    <label for="last_name">Last Name</label>
-  </div>
-  
-  <div>
-    <input type="email" id="email" name="email" required placeholder=" " />
-    <label for="email">Email Address</label>
-    <div class="requirements">
-      Must be a valid email address.
-    </div>
-  </div>
-  
-  <div>
-    <input type="password" id="password" name="password" required placeholder=" " pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}" />
-    <label for="password">Password</label>
-    <div class="requirements">
-      Your password must be at least 6 characters as well as contain at least one uppercase, one lowercase, and one number.
-    </div>
+  <div class="check">
+    <label for="sms">Не отправлять СМС:</label>
+    <input class="check__input" type="checkbox" name="sms" required placeholder=" " />
   </div>
   
   <input type="submit" value="Sign Up" />
@@ -69,20 +102,25 @@ form
     border-bottom: 1px solid #ccc
 
     > label 
-      opacity: 0.3
+      opacity: 0.4
       font-weight: bold
       position: absolute
       top: 22px
       left: 20px
-    
-    > input[type="text"],
-    > input[type="email"],
-    > input[type="password"]
+
+    > input,
+      textarea
+        font-size: 1.2rem
+        padding: 20px 20px 20px 50px
+        opacity: 0.8
+
+    > input
       width: 100%
       border: 0
       padding: 20px 20px 20px 50px
       background: #eee
       
+
       &:focus 
         outline: 0
         background: white
@@ -126,6 +164,42 @@ form
     border: 0
     padding: 20px
     font-size: 1.2rem
+
+  .check
+    padding: 30px
+    background: white
+    >label
+      width: 100%
+      padding: 20px
+      opacity: 0.8
+      top:0
+      left:0
+
+    >.select__input
+      position: absolute
+      right: 20px
+      top: 0
+      width: 40%
+      height: 60px
+      border: none
+
+      &:focus
+        outline: none
+
+    >.select__input,
+      textarea
+        opacity: 0.8
+        font-size: 1.2rem
+
+
+    >.check__input
+      position: absolute
+      margin:0
+      right:20px
+      top: 15px
+      width: 30px
+      height: 30px
+        
     
 * 
   box-sizing: border-box
